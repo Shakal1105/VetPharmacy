4ff0d385fcd7a15417c07e87d0edac70904fc6c4
Shakal1105 <55797859+Shakal1105@users.noreply.github.com>
2022-07-13 23:35:22 +0300

    Update telebotenot.py

--- telebotenot.py
+++ telebotenot.py
@@ -91,83 +91,85 @@ class Bot():
 
         @bot.callback_query_handler(func=lambda callback_query: True)
         def callback(callback_query: CallbackQuery):
-            callback = callback_query.data
-            if callback == 'hel' or callback == "back":
-                bot.answer_callback_query(callback_query.id, text="Для звичайних користувачів плагін в розробці\n\nякщо ви є адміністратором нажміть адміністратор", show_alert=True)
-                bot.edit_message_text(chat_id=callback_query.message.chat.id,text="Оберіть хто ви", message_id=callback_query.message.id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Користувач", callback_data='user'), InlineKeyboardButton(text="Адміністратор", callback_data="admin")]]))
-            if callback_query.from_user.id in self.admins:
-                if callback == 'admin':
-                    bot.answer_callback_query(callback_query.id, text="Команди:\n +сумма\n`=` - загальна сумма\nadd user_id - дать права администраторa\ndel user_id - забрать права администратора", show_alert=True)
-                    bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.id, text="Оберіть те що вам потрібно", reply_markup=self.keyboard)
-                if callback == "sum":
-                    bot.answer_callback_query(callback_query.id,text="Для того щоб вносити грощі до нотатки потрібно написати + сума наприклад\n+123\n\nякщо по терміналу в кінці обов'язково  +\n+1823+\n\nЯкщо копійки\n+194.13  - обовьязково крапка",show_alert=True)
-                elif callback == 'products':
-                    self.tovars_text = ''
-                    file = open('tovar.txt', 'r', encoding = "utf-8")
-                    tovaru = []
-                    for tov in file:
-                        tovaru.append(tov)
-                        stroka = tov.replace("\n", " ")
-                        arr = stroka.split(" ")
-                        self.tovars[arr[0]] = {"price": arr[1], "num": arr[2]}
-                    file.close()
-                    for line in tovaru:
-                        self.tovars_text = self.tovars_text + line
-                    bot.send_message(callback_query.message.chat.id, u"{}".format(self.tovars_text))
-                elif callback == 'users':
-                    text = ''
-                    for us in self.us:
-                        text = text + us + '\n'
-                    bot.send_message(callback_query.message.chat.id, text)
-                elif callback == 'money':
-                    self.tovars_text = ""
-                    file = open('price.txt', 'r')
-                    for line in file:
-                        self.tovars_text = self.tovars_text + line
-                    bot.send_message(callback_query.message.chat.id, self.tovars_text)
-                elif callback == "yesdel":
-                    self.prices = []
-                    self.price = []
-                    file = open('prices.txt', 'w')
-                    file.write('')
-                    file.close()
-                    file = open('price.txt', 'w')
-                    file.write('0' + '\n')
-                    file.close()
-                    bot.send_message(callback_query.message.chat.id, "Історія продажу видалена")
-                    bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.id,text="Оберіть те що вам потрібно", reply_markup=self.keyboard)
-                elif callback == "clear":
-                    bot.edit_message_text(chat_id=callback_query.message.chat.id, text="Ви точно хочете видалити історію продажу?", message_id=callback_query.message.id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Так",callback_data='yesdel'),InlineKeyboardButton(text="Ні",callback_data="admin")]]))
-                elif callback == "=":
-                    arr = []
-                    terminal = 0
-                    kasa = 0
-                    self.sum = 0
-                    for num in self.prices:
-                        self.sum = self.sum + float(num)
-                    file = open('price.txt', "r")
-                    for line in file:
-                        arr.append(line.strip())
-                    file.close()
-                    for element in arr:
-                        long = len(element)-8
-                        if element[long:] == "Terminal":
-                            terminal=terminal + float(element[:long])
-                        else:
-                            kasa=kasa+float(element)
-                    bot.send_message(callback_query.message.chat.id, "Загальна сума ==> {} грн\nТермінал ==> {} грн\nКаса ==> {} грн".format(self.sum,terminal,kasa))
+            try:
+                callback = callback_query.data
+                if callback == 'hel' or callback == "back":
+                    bot.answer_callback_query(callback_query.id, text="Для звичайних користувачів плагін в розробці\n\nякщо ви є адміністратором нажміть адміністратор", show_alert=True)
+                    bot.edit_message_text(chat_id=callback_query.message.chat.id,text="Оберіть хто ви", message_id=callback_query.message.id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Користувач", callback_data='user'), InlineKeyboardButton(text="Адміністратор", callback_data="admin")]]))
+                if callback_query.from_user.id in self.admins:
+                    if callback == 'admin':
+                        bot.answer_callback_query(callback_query.id, text="Команди:\n +сумма\n`=` - загальна сумма\nadd user_id - дать права администраторa\ndel user_id - забрать права администратора", show_alert=True)
+                        bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.id, text="Оберіть те що вам потрібно", reply_markup=self.keyboard)
+                    if callback == "sum":
+                        bot.answer_callback_query(callback_query.id,text="Для того щоб вносити грощі до нотатки потрібно написати + сума наприклад\n+123\n\nякщо по терміналу в кінці обов'язково  +\n+1823+\n\nЯкщо копійки\n+194.13  - обовьязково крапка",show_alert=True)
+                    elif callback == 'products':
+                        self.tovars_text = ''
+                        file = open('tovar.txt', 'r', encoding = "utf-8")
+                        tovaru = []
+                        for tov in file:
+                            tovaru.append(tov)
+                            stroka = tov.replace("\n", " ")
+                            arr = stroka.split(" ")
+                            self.tovars[arr[0]] = {"price": arr[1], "num": arr[2]}
+                        file.close()
+                        for line in tovaru:
+                            self.tovars_text = self.tovars_text + line
+                        bot.send_message(callback_query.message.chat.id, u"{}".format(self.tovars_text))
+                    elif callback == 'users':
+                        text = ''
+                        for us in self.us:
+                            text = text + us + '\n'
+                        bot.send_message(callback_query.message.chat.id, text)
+                    elif callback == 'money':
+                        self.tovars_text = ""
+                        file = open('price.txt', 'r')
+                        for line in file:
+                            self.tovars_text = self.tovars_text + line
+                        bot.send_message(callback_query.message.chat.id, self.tovars_text)
+                    elif callback == "yesdel":
+                        self.prices = []
+                        self.price = []
+                        file = open('prices.txt', 'w')
+                        file.write('')
+                        file.close()
+                        file = open('price.txt', 'w')
+                        file.write('0' + '\n')
+                        file.close()
+                        bot.send_message(callback_query.message.chat.id, "Історія продажу видалена")
+                        bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.id,text="Оберіть те що вам потрібно", reply_markup=self.keyboard)
+                    elif callback == "clear":
+                        bot.edit_message_text(chat_id=callback_query.message.chat.id, text="Ви точно хочете видалити історію продажу?", message_id=callback_query.message.id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Так",callback_data='yesdel'),InlineKeyboardButton(text="Ні",callback_data="admin")]]))
+                    elif callback == "=":
+                        arr = []
+                        terminal = 0
+                        kasa = 0
+                        self.sum = 0
+                        for num in self.prices:
+                            self.sum = self.sum + float(num)
+                        file = open('price.txt', "r")
+                        for line in file:
+                            arr.append(line.strip())
+                        file.close()
+                        for element in arr:
+                            long = len(element)-8
+                            if element[long:] == "Terminal":
+                                terminal=terminal + float(element[:long])
+                            else:
+                                kasa=kasa+float(element)
+                        bot.send_message(callback_query.message.chat.id, "Загальна сума ==> {} грн\nТермінал ==> {} грн\nКаса ==> {} грн".format(self.sum,terminal,kasa))
+                    elif callback == "exit":
+                        bot.delete_message(callback_query.message.chat.id, callback_query.message.id)
+                        bot.send_message(callback_query.message.chat.id, text="Гарного вам дня та успіху 🦝")
+                if callback == "user":
+                    bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.id, text="Можливості користувача:", reply_markup=self.keyboard_user)
                 elif callback == "exit":
-                    bot.delete_message(callback_query.message.chat.id, callback_query.message.id)
-                    bot.send_message(callback_query.message.chat.id, text="Гарного вам дня та успіху 🦝")
-            if callback == "user":
-                bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.id, text="Можливості користувача:", reply_markup=self.keyboard_user)
-            elif callback == "exit":
-                bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.id, text="Гарного вам дня та успіху 🦝")
-        try:
-            bot.polling()
-        except Exception as e:
-            print(e)
-            time.sleep(11)
-            self.vet()
+                    bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.id, text="Гарного вам дня та успіху 🦝")
+            
+            except Exception as e:
+                print(e)
+                callback="error"
+                pass
+        bot.polling()
+        
 if __name__ == "__main__":
     Bot()
